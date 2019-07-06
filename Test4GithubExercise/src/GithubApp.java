1eaa8e615d11c8224e31ed817b4ae128a4e7d494
Kin-AppDev <52585291+Kin-AppDev@users.noreply.github.com>
2019-07-05 21:58:39 -0500

    Add files via upload

---
+++ Test4GithubExercise/src/GithubApp.java
@@ -0,0 +1,29 @@
+//Just a program for the Github exercise of git basic commands
+
+public class GithubApp {
+	public static void main(String[] args) {
+
+		double a, b, x, y, answer;
+		a = 10;
+		b = 20;
+		x = 50;
+		y = 40;
+			
+	//Add
+		answer = x + y;
+		System.out.println("Answer for addition = " + answer);
+		
+	//Subtract
+		answer = x - y;
+		System.out.println("Answer for subtraction = " + answer);
+		
+	//Multiply
+		answer = x * y;
+		System.out.println("Answer for multiplication = " + answer);
+		
+	//Divide
+		answer = x / y;
+		System.out.println("Answer for division = " + answer);
+			
+	}
+}
